4c19a4cb362f91cc4bc0294debd35ce4e571b49b
Mario <mario@local>
2025-12-12 14:25:35 +0100

    MAUI: elimina configuraciÃ³n de fuentes inexistentes

--- src/Frontend/MauiApp/MauiProgram.cs
+++ src/Frontend/MauiApp/MauiProgram.cs
@@ -7,12 +7,7 @@ public static class MauiProgram
 	{
 		var builder = Microsoft.Maui.Hosting.MauiApp.CreateBuilder();
 		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			.UseMauiApp<App>();
 
 		return builder.Build();
 	}
